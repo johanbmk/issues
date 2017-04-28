7dc0f7b37dbd1fb38cddddeef0d5b98128006041
Johan Brattemark <johan.brattemark@gmail.com>
2017-04-28 11:44:10 -0700

    Implement getter 'totalComments'

--- Issues.js
+++ Issues.js
@@ -50,7 +50,8 @@ class Issues {
   // return an array containing all the `id` property of the `assignedIssue`
   // property for each issue with a `pull_request` property that is not `null`
   get withPullRequest() {
-    return issues.filter(obj => obj.pull_request !== undefined && obj.pull_request !== null)
+    return issues.filter(obj => obj.pull_request !== undefined
+      && obj.pull_request !== null)
     .map(obj => obj.id);
   }
 
